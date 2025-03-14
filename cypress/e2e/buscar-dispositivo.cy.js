70aefc65f416c932ef9858a40d67ae8bd98e0f04
andrezambroni <andrezambroni@gmail.com>
2025-03-14 16:45:07 -0300

    cadastrar dispositivo

--- cypress/e2e/buscar-dispositivo.cy.js
+++ cypress/e2e/buscar-dispositivo.cy.js
@@ -2,13 +2,45 @@
 
 describe("Buscar dispositivo", () => {
   it("Buscar dispositivo especifico", () => {
-    cy
+    cy.request({
+      method: "GET",
+      url: "https://api.restful-api.dev/objects/7",
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body.id).to.eq("7")
+    })
   })
 
   it("Buscar dispositivo inexistente", () => {
-
+    cy.request({
+      method: "GET",
+      url: "https://api.restful-api.dev/objects/dfgdfg",
+      body: {},
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(404)
+      expect(response.body.error).to.eq("Oject with id=dfgdfg was not found.")
+    })
   })
 
-
-
+  it("Cadastrar dispositivo", () => {
+    cy.request({
+      method: "POST",
+      url: "https://api.restful-api.dev/objects",
+      body: {
+        name: "Device AZ",
+        data: {
+          year: "2025",
+          price: 10000,
+          "CPU Model": "R7 5800X3D",
+          "GPU Model": "RTX 4060 TI",
+          RAM: "32GB",
+        },
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body.name).to.equal("Device AZ");
+    })
+  })
 })
